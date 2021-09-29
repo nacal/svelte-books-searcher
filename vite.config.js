579f0c096e2b0f40ac1337b561e23d094e4ed51a
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-09-29 16:46:35 +0900

    edit vite config for alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import * as path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  alias: {
+    // the key must start and end with a slash
+    '/@assets/': path.join(__dirname, '/src/assets/'),
+    '/@components/': path.join(__dirname, '/src/components/'),
+    '/@store/': path.join(__dirname, '/src/store/'),
+  }
 });
