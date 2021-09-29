3bc5a8548ec7eb6b701ac0086bdac2104a0e9bfd
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-09-29 16:47:06 +0900

    develop header component

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,8 @@
 <script lang="ts">
-  import logo from "./assets/svelte.png";
   import Tailwind from "./Tailwind.svelte";
+  import { Header } from "./components";
 </script>
 
 <Tailwind />
-
-<main class="text-center p-4 mx-0">
-  <img
-    width="100"
-    height="100"
-    src={logo}
-    alt="Svelte Logo"
-    class="inline-block"
-  />
-</main>
+<Header />
+<main class="text-center p-16 mx-0" />
---
+++ src/components/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+  import logo from "/@assets/svelte.png";
+</script>
+
+<header
+  class="top-0 lef-0 w-full h-16 z-40 bg-gray-900 shadow fixed border-b border-gray-200 flex justify-center items-center "
+>
+  <h1 class="font-semibold text-xl text-white tracking-tight">
+    <img
+      width="48"
+      height="48"
+      src={logo}
+      alt="Svelte Logo"
+      class="inline-block"
+    />
+  </h1>
+</header>
