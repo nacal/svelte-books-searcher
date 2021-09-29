167acb3b254416fa16e89f5dfc7b7d3bab6dd613
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-09-29 17:01:44 +0900

    develop svelte-routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+  import { Router, Route } from "svelte-routing";
   import Tailwind from "./Tailwind.svelte";
   import { Header } from "./components";
+  import { Home, SearchBooks } from "./pages";
 </script>
 
 <Tailwind />
 <Header />
-<main class="text-center p-16 mx-0" />
+<Router>
+  <main class="text-center p-16 mx-0">
+    <Route path="" component={Home} />
+    <Route path="search-books" component={SearchBooks} />
+  </main>
+</Router>
